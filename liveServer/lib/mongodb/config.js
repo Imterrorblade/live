1254dbfb43dc63a4050a097a1c1fc62f001e62b9
yangcl <210485393@qq.com>
2018-12-04 18:51:46 +0800

    添加mongodb服务

---
+++ liveServer/lib/mongodb/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  user: '',//用户名
+  password: '',
+  url: 'mongodb://localhost/live'//数据库地址
+}
---
+++ liveServer/lib/mongodb/index.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose')
+
+var config = require('./config.js')
+
+//连接数据库
+mongoose.connect(url)
+
+//获取数据连接
+const connect = mongoose.connection
+
+
+//监听数据库连接
+connect.on('error',err => {
+  console.log(err)
+})
+
+connect.on('connnected', () => {
+  console.log('mongodb 已连接')
+})
+
+connect.on('disconnected', () => {
+  console.log('mongodb 断开连接')
+})
+
+connect.on('open', ()=> {
+  console.log('mongodb 打开')
+})
+
+module.exports = mongoose
