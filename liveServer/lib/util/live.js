dc853fdf65ec3b66771ac04875bfc328d0244894
yangcl <210485393@qq.com>
2018-12-12 13:55:59 +0800

    添加流媒体服务，和推流模块ffmpeg

--- liveServer/lib/util/live.js
+++ liveServer/lib/util/live.js
@@ -1,20 +1,80 @@
 const fs = require('fs')
 const path = require('path')
+const ffmpeg = require('fluent-ffmpeg')
 
 module.exports = {
   pushVideo(fileName, data) {
-    const array = Object.keys(data).map(key =>{
+    const _self = this
+    const array = Object.keys(data).map(key => {
       return data[key]
     })
     const buf = new Uint8Array(array)
+    const filepath = path.resolve('public/assets/video', fileName + '.webm')
+    const outputPath = 'http://localhost:3001/live/' + fileName
     fs.writeFile(
-      path.resolve('public/assets/video', fileName + '.webm'),
+      filepath,
       buf,
-      function(err) {
+      function (err) {
         if (err) {
           console.log(err)
         }
+        this.pushStream(filepath, outputPath)
       }
     )
-  }
+  },
+  pushStream(inputPath, outputPath) {
+    console.log('推流开始')
+    ffmpeg(inputPath)
+      .inputOptions('-re')
+      .inputOptions('-ac 2')
+      .addInput('./bin/logo.png')
+      .complexFilter([{
+          filter: 'scale',
+          options: [1080, -1],
+          inputs: '[0:v]',
+          outputs: 'c'
+        },
+        {
+          filter: 'scale',
+          options: [200, -1],
+          inputs: '[1:v]',
+          outputs: 'logo'
+        },
+        {
+          filter: 'overlay',
+          options: {
+            x: 'main_w-overlay_w-5',
+            y: 5
+          },
+          inputs: ['c', 'logo'],
+          outputs: ['output', 'a']
+        }
+      ], 'output')
+      .on('start', function (commandLine) {
+        console.log('Spawned Ffmpeg with command: ' + commandLine);
+      })
+      .on('error', function (err, stdout, stderr) {
+        console.log('error: ' + err.message);
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+      })
+      .on('end', function () {
+        console.log('Processing finished !');
+      })
+      .addOptions([
+        '-vcodec libx264',
+        '-preset veryfast',
+        '-crf 22',
+        '-maxrate 1000k',
+        '-bufsize 3000k',
+        '-acodec libmp3lame',
+        '-ac 2',
+        '-ar 44100',
+        '-b:a 96k'
+      ])
+      .format('flv')
+      .pipe(outputPath, {
+        end: true
+      })
+  },
 }
